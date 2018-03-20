e5218f3a763fe50155e025b4aba150b9ee80d1f2
nanjizal <none>
2018-03-20 10:00:32 +0000

    removed outline from TriangleGradient its an aspect no longer relevant

--- src/trilateral/tri/TriangleGradient.hx
+++ src/trilateral/tri/TriangleGradient.hx
@@ -4,14 +4,13 @@ import trilateral.tri.TriangleArray;
 import trilateral.geom.Algebra;
 abstract TriangleGradient( Triangle ) from Triangle to Triangle {
     public inline function new(  id_: Int
-                            , outline_: Bool
                             , A_: Point, B_: Point, C_: Point
                             , depth_: Float
                             , colorID_: Int
                             , colorID2_: Int
                             , gradCorner_: Int
                             ){
-            this = new Triangle( id_, outline_, A_, B_, C_, depth_, colorID_ );
+            this = new Triangle( id_, A_, B_, C_, depth_, colorID_ );
             switch( gradCorner_ ){
                 case 0:
                     this.colorA = colorID2_;
@@ -40,7 +39,6 @@ abstract TriangleGradient( Triangle ) from Triangle to Triangle {
             }
     }
     public inline static function quadGradient( id_:        Int
-                                            ,   outline_:   Bool
                                             ,   pos_:       Point, dim_: Point
                                             ,   depth_:     Float
                                             ,   colorID_:   Int
@@ -54,11 +52,11 @@ abstract TriangleGradient( Triangle ) from Triangle to Triangle {
         //   D   C
         var line = Algebra.rotateVectorLine( pos, dim, theta, pivotX, pivotY );
         if( horizontal ){
-            return { t0: new TriangleGradient( id_, outline_, line.A, line.B, line.D, depth_, colorID_, colorID2_, 1 )
-                ,    t1: new TriangleGradient( id_, outline_, line.B, line.C, line.D, depth_, colorID2_, colorID_, 2 ) };
+            return { t0: new TriangleGradient( id_, line.A, line.B, line.D, depth_, colorID_, colorID2_, 1 )
+                ,    t1: new TriangleGradient( id_, line.B, line.C, line.D, depth_, colorID2_, colorID_, 2 ) };
         } else {
-            return { t0:  new TriangleGradient( id_, outline_, line.A, line.B, line.D, depth_, colorID_, colorID2_, 2 )
-                ,    t1:  new TriangleGradient( id_, outline_, line.B, line.C, line.D, depth_, colorID2_, colorID_, 0 ) };
+            return { t0:  new TriangleGradient( id_, line.A, line.B, line.D, depth_, colorID_, colorID2_, 2 )
+                ,    t1:  new TriangleGradient( id_, line.B, line.C, line.D, depth_, colorID2_, colorID_, 0 ) };
         }
     }
     public static inline function multiGradient(    id_:    Int,    horizontal_:    Bool
@@ -83,14 +81,14 @@ abstract TriangleGradient( Triangle ) from Triangle to Triangle {
             var dim = { x: step, y: -hi };
             for( i in 0...loops ){
                 var pos = { x: left + i*step, y: top };
-                triangles.pushPair( TriangleGradient.quadGradient( id_, false, pos, dim, 0, colors[ i ], colors[ i + 1 ], horizontal_, theta, pivotX, pivotY ) );
+                triangles.pushPair( TriangleGradient.quadGradient( id_, pos, dim, 0, colors[ i ], colors[ i + 1 ], horizontal_, theta, pivotX, pivotY ) );
             }
         } else {
             var step: Float = hi/sections;
             var dim = { x: wid, y: -step };
             for( i in 0...loops ){
                 var pos = { x: left, y: top - i*step };
-                triangles.pushPair( TriangleGradient.quadGradient( id_, false, pos, dim, 0, colors[ i ], colors[ i + 1 ], horizontal_, theta, pivotX, pivotY ) );
+                triangles.pushPair( TriangleGradient.quadGradient( id_, pos, dim, 0, colors[ i ], colors[ i + 1 ], horizontal_, theta, pivotX, pivotY ) );
             }
         }
     }
