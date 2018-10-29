4c8a0ae79f2dd9ccec2b1696e37f534ce2130cbc
nanjizal <nanjizal@users.noreply.github.com>
2018-10-29 16:27:02 +0000

    fix haxe4 issue

--- src/trilateral/angle/ZeroToMinus2pi.hx
+++ src/trilateral/angle/ZeroToMinus2pi.hx
@@ -2,8 +2,7 @@ package trilateral.angle;
 import trilateral.angle.Fraction;
 @forward
 abstract ZeroToMinus2pi( Float ) to Float {
-    // private can't create directly
-    inline 
+    inline // private can't create directly
     function new( f: Float ){
         this = f;
     }
@@ -19,67 +18,40 @@ abstract ZeroToMinus2pi( Float ) to Float {
     @:op(A <= B) static function lte( a:ZeroToMinus2pi, b:ZeroToMinus2pi ) : Bool;
     @:op(A + B)
     public function additionPi( b: ZeroToMinus2pi ): ZeroToMinus2pi {
-        var f: Float = this + b;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) + ( b: Float ); 
     }
     @:op(A - B)
     public function subtractionPi( b: ZeroToMinus2pi ): ZeroToMinus2pi {
-        var f: Float = this;
-        var f2: Float = b;
-        f -= f2;
-        var p: ZeroToMinus2pi = f;
-        return f;
+        return ( this: Float) - ( b: Float );
     }
     @:op(A / B)
     public function dividePi( b: ZeroToMinus2pi ): ZeroToMinus2pi {
-        var f: Float = this;
-        var f2: Float = b;
-        f /= f2;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) / ( b: Float ); 
     }
     @:op(A * B)
     public function timesPi( b: ZeroToMinus2pi ): ZeroToMinus2pi {
-        var f: Float = this;
-        var f2: Float = b;
-        f *= f2;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) * ( b: Float );
     }
     @:op(A + B)
     public function addition( b: Float ): ZeroToMinus2pi {
-        var f: Float = this;
-        f += b;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) + b; 
     }
     @:op(A - B)
     public function subtraction( b: Float ): ZeroToMinus2pi {
-        var f: Float = this;
-        f -= b;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) - b; 
     }
     @:op(A / B)
     public function divide( b: Float ): ZeroToMinus2pi {
-        var f: Float = this;
-        f /= b;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) / b; 
     }
     @:op(A * B)
     public function times( b: Float ): ZeroToMinus2pi {
-        var f: Float = this;
-        f *= b;
-        var p: ZeroToMinus2pi = f;
-        return f; 
+        return ( this: Float ) * b; 
     }
     public var degrees( get, set ): Float;
     public inline 
     function get_degrees(): Float{
-        var f: Float = this;
-        return f*180/Math.PI;
+        return ( this: Float )*180/Math.PI;
     }
     public inline 
     function set_degrees( val:Float ): Float {
@@ -89,18 +61,21 @@ abstract ZeroToMinus2pi( Float ) to Float {
     @:from
     inline static
     function fromFraction( val: Fraction ):ZeroToMinus2pi {
-        return new ZeroToMinus2pi( val.toFloat()*Math.PI );
+        return val.toFloat()*Math.PI;
     }
     @:to
     inline function tofraction(): Fraction {
-        var f: Float = this;
-        var frac: Fraction = this/Math.PI;
-        return frac;
+        return ( this: Float )/Math.PI;
     }
     @:from
     inline static
     function fromString( val: String ):ZeroToMinus2pi {
         var frac: Fraction = val;
-        return new ZeroToMinus2pi( frac.toFloat()*Math.PI );
+        return frac.toFloat()*Math.PI;
+    }
+    @:to
+    inline public
+    function toString(): String {
+        return Std.string( ( this: Float ) );
     }
 }
