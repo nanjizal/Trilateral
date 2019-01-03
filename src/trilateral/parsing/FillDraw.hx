f91dd1a56b8a7129e26cb22ce8eeb5325fd2bd0f
nanjizal <nanjizal@users.noreply.github.com>
2019-01-03 04:16:47 +0000

    push more functionality into FillDraw to simplify inherited.

--- src/trilateral/parsing/FillDraw.hx
+++ src/trilateral/parsing/FillDraw.hx
@@ -1,8 +1,12 @@
 package trilateral.parsing;
 import trilateral.tri.TriangleArray;
 import trilateral.tri.Triangle;
-import trilateral.path.Fine;
+import trilateral.arr.ArrayTriple;
 import trilateral.path.Base;
+typedef TfillDatas = {
+    var vert: Array<Float>;
+    var tri:  Array<Int>;
+}
 class FillDraw {
     public var count        = 0;
     public var colors       = new Array<Int>();
@@ -16,16 +20,39 @@ class FillDraw {
     }
     public
     function fill( p: Array<Array<Float>>, colorID: Int ){
-        throw 'please extend FillDraw with implementation';
+        var fillDatas = fillFunc( p );
+        return iterFill( fillDatas.vert, fillDatas.tri, colorID );
     }
     // used to help show random fills
     public
     function fillRnd( p: Array<Array<Float>>, rnd: Int ){
-        throw 'please extend FillDraw with implementation';
+        var fillDatas = fillFunc( p );
+        return iterFill( fillDatas.vert, fillDatas.tri, rnd, true );
+    }
+    public
+    function fillFunc( p: Array<Array<Float>> ):TfillDatas {
+        return { vert: [], tri: [] }
     }
     public
     function pathFactory(): Base {
         throw 'please extend FillDraw with implementation';
+    }
+    inline function rndInt( rnd: Int ){
+        return Std.int( Math.round( Math.random()*rnd ) );
+    }
+    function iterFill( vert: Array<Float>, tri: Array<Int>, colorID: Int, ifRnd: Bool = false ){
+        var triples = new ArrayTriple( tri );
+        var i: Int;
+        var id: Int;
+        for( tri in triples ){
+            var a: Int = Std.int( tri.a*3 );
+            var b: Int = Std.int( tri.b*3 );
+            var c: Int = Std.int( tri.c*3 );
+            id = ( ifRnd )? colorID: rndInt( colorID );
+            triangles.drawTriangle(  count, { x: vert[ a ], y: vert[ a + 1 ] }
+                                , { x: vert[ b ], y: vert[ b + 1 ] }
+                                , { x: vert[ c ], y: vert[ c + 1 ] }, id );
+        }
     }
     public inline
     function colorId( color: Int ): Int {
